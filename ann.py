48222dd36ef7647333862282c5cbf99755cd8251
Sachin <sachinwork91@gmail.com>
2017-07-30 17:50:46 -0400

    Updated with GridSearchCV for Optimization

--- ann.py
+++ ann.py
@@ -47,6 +47,7 @@ X_test = sc.transform(X_test)
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 
 # Initialising the ANN
 #We don not add anything here as we will be adding layers to our classifier(Neural Network)
@@ -58,12 +59,12 @@ classifier = Sequential()
 #After specifying the input_dim at the first layer we do not need to specify again at the second layer
 #as it can know from the output dimention of the first layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
-
+classifier.add(Dropout(p=0.1))
 # Adding the second hidden layer
 #notice we are using recifier function as the activation function for hidden layers
 #sigmoid function will be used in the final output layer
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
-
+classifier.add(Dropout(p=0.1))
 # Adding the output layer
 #sigmoid functions gives the probablity, so in this case we have two classes
 #i.e whether the user will leave or stay
@@ -108,6 +109,7 @@ classifier.predict(X_sample)
 # Evaluating the ANN
 import tensorflow
 import keras
+from sklearn.model_selection import GridSearchCV
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import cross_val_score
 from keras.models import Sequential
@@ -121,7 +123,7 @@ def build_classifier():
     classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
     classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
     return classifier
-if __name__ == "__main__":
+
     classifier = KerasClassifier(build_fn = build_classifier, batch_size = 100, epochs = 100)
     accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = 10)
     mean = accuracies.mean()
@@ -129,4 +131,36 @@ if __name__ == "__main__":
     print("\nfinished")
     print("Mean: ", mean)
     print("Variance : ",variance)
+    
+    
+    
+    
+###########################     Model Evaluation using GRID SEARCH BY SKLEARN ###############################
+def build_classifier_all_optimization(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer =optimizer , loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+
+
+model = KerasClassifier(build_fn = build_classifier_all_optimization, batch_size = 100, epochs = 100)
+  
+# define the grid search parameters
+optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+param_grid = dict(optimizer=optimizer)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1)
+grid_result = grid.fit(X, y)
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))    
+    
+    
+    
+    
+    
     
